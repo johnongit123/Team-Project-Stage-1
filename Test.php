9a57fe495e3abf92764d8142bc436048a86ff866
OthmanG7798 <151866595+OthmanG7798@users.noreply.github.com>
2024-02-09 20:15:05 +0000

    Add files via upload

---
+++ Test.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "Localhost"; // Change this to your MySQL server address
+$username = "team20"; // Change this to your MySQL username
+$password = "password"; // Change this to your MySQL password
+$database = "Team20"; // Change this to the name of your MySQL database
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+// Close connection
+$conn->close();
+?>
