30d73e373bacfda0e9d5f5c254035c7fe767cc73
johnongit123 <147746987+johnongit123@users.noreply.github.com>
2024-02-10 18:30:57 +0000

    Delete js directory

--- js/logout_scripts.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('logoutLink').addEventListener('click', function(e) {
-        e.preventDefault();
-        sessionStorage.removeItem('isLoggedIn');
-        window.location.href = 'landing.html';
-});
