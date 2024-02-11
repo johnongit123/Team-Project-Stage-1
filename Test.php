93f21efc86e328482a8b709eb76c94f490dd077d
OthmanG7798 <151866595+OthmanG7798@users.noreply.github.com>
2024-02-11 17:39:44 +0000

    Update Test.php

--- Test.php
+++ Test.php
@@ -17,3 +17,38 @@ echo "Connected successfully";
 // Close connection
 $conn->close();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Link to your CSS file -->    
+</head>
+
+    <div class="top-bar">
+        <h1>
+            My Forum
+        </h1>
+    </div>
+    <div class="main">
+        <ol>  
+            <li class ="row">
+                <a href="/thread.html?id=${thread.id}">
+                    <h4 class = "title">
+                        ${thread.title} 
+                    </h4>
+                    <div class = "bottom">
+                        <p class = "timestamp">
+                            ${new Date(thread.date).toLocaleString()}
+                        </p>
+                        <p class="comment-count">
+                             _${thread.comments.length} comments
+                        </p>
+                    </div>
+                </a>
+
+        </ol>
+    </div>
+
+    <script>
