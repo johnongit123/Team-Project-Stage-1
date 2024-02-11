08ec25d9cb18deb542b25c73f93f8f15aee57cc6
OthmanG7798 <151866595+OthmanG7798@users.noreply.github.com>
2024-02-11 18:00:58 +0000

    Add files via upload

--- Test.php
+++ Test.php
@@ -1,2 +1,39 @@
+<?php
+// Establish a connection to your SQL database
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database_name";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Execute query to fetch data
+$sql = "SELECT id, title, date, comments FROM threads";
+$result = $conn->query($sql);
+
+// Check if there are rows returned
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        // Generate HTML dynamically using fetched data
+        echo '<li class="row">
+                <a href="/thread.html?id=' . $row["id"] . '">
+                    <h4 class="title">' . $row["title"] . '</h4>
+                    <div class="bottom">
+                        <p class="timestamp">' . date("Y-m-d H:i:s", strtotime($row["date"])) . '</p>
+                        <p class="comment-count">' . count(json_decode($row["comments"])) . ' comments</p>
+                    </div>
+                </a>
+              </li>';
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
