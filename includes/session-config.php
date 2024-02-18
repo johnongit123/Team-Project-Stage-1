6691fff74a565b27156a20e84ccd138096d589b1
johnongit123 <147746987+johnongit123@users.noreply.github.com>
2024-02-18 17:40:28 +0000

    Update session-config.php

--- includes/session-config.php
+++ includes/session-config.php
@@ -23,26 +23,43 @@ function check_login() {
     }
 }
 
-function regen_session_id_loggedin($loginId){
+
+
+
+if (isset($_SESSION["login_id"])) {
+    if(!isset($_SESSION["last_regeneration"])){
+        regen_session_id_loggedin();
+    } else{
+        $interval = 60 * 30;
+        if (time() - $_SESSION["last_regeneration"] >= $interval) {
+            regen_session_id_loggedin();
+        }
+    }
+} else {
+    if(!isset($_SESSION["last_regeneration"])){
+        regen_session_id();
+    } else {
+        $interval = 60 * 30;
+        if (time() - $_SESSION["last_regeneration"] >= $interval) {
+            regen_session_id();
+        }
+    }
+}
+
+
+
+function regen_session_id_loggedin(){
     session_regenerate_id(true);
+    
     $loginId = $_SESSION["login_id"];
     $newSessionId = session_create_id();
     $sessionId = $newSessionId . "_". $loginId;
     session_id($sessionId);
+
     $_SESSION["last_regeneration"] = time();
 }
 
 function regen_session_id(){
     session_regenerate_id(true);
     $_SESSION["last_regeneration"] = time();
-}
-
-if (isset($_SESSION["login_id"])) {
-    if (!isset($_SESSION["last_regeneration"]) || time() - $_SESSION["last_regeneration"] >= 60 * 30) {
-        regen_session_id_loggedin($_SESSION["login_id"]);
-    }
-} else {
-    if (!isset($_SESSION["last_regeneration"]) || time() - $_SESSION["last_regeneration"] >= 60 * 30) {
-        regen_session_id();
-    }
 }
