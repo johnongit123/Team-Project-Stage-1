d0830683452e3546053d11fd44e25ada011eafd4
johnongit123 <147746987+johnongit123@users.noreply.github.com>
2024-02-18 14:40:45 +0000

    Update dbh.php

--- includes/dbh.php
+++ includes/dbh.php
@@ -12,20 +12,6 @@ try {
     // Check if the connection was successful
     if (!$con){
         throw new Exception("Connection failed: " . mysqli_connect_error());
-    } else{
-        echo "we connected!";
-        $query = "SELECT * from employee";
-        $result = $con->query($query);
-        // Check if the query was successful
-        if ($result) {
-            // Fetch data from the result object and display it
-            while ($row = $result->fetch_assoc()) {
-                // Assuming 'employee' table has a column named 'name'
-                echo "Employee Name: " . $row['first_name'] . "<br>";
-            }
-        } else {
-            echo "Error executing query: " . $con->error;
-        }
     }
 } catch(Exception $e) {
     // Connection failed, handle the exception
