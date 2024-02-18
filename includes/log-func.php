e045b492a97f88ede6f1734e9ee851e454fd537f
johnongit123 <147746987+johnongit123@users.noreply.github.com>
2024-02-18 17:39:52 +0000

    Update log-func.php

--- includes/log-func.php
+++ includes/log-func.php
@@ -24,25 +24,12 @@ function is_email_wrong(string $email) {
 
 //Valid password (does it match database)
 function is_password_wrong(string $pwd, string $email) {
-    $pwd_h_db = get_password($email);
+    $hashed_password = get_password($email);
 
-    if($pwd_h_db === null || !password_verify($pwd, $pwd_h_db)) {
+    if($hashed_password === null || !password_verify($pwd, $hashed_password)) {
         return true;
     } else {
         return false;
     }
 }
-
-//retreive login id from database
-function validate_login(string $email, string $password) {
-    $hashed_password = get_password($email);
-
-    if ($hashed_password && password_verify($password, $hashed_password)) {
-            $login_id = collect_login_id($email);
-            return $login_id;
-    }
-    return false;
-}
-
-
 
