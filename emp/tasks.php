cd29edfccbb50e15a2d1628703f3ad4ebdd459f9
johnongit123 <147746987+johnongit123@users.noreply.github.com>
2024-02-19 00:21:32 +0000

    Update tasks.php

--- emp/tasks.php
+++ emp/tasks.php
@@ -52,13 +52,6 @@ check_login();
         </div>
         <ul class="sidebar-list">
             <li class="sidebar-list-item">
-                <a href="emp_dash.php">
-                    <span class="material-symbols-outlined">dashboard</span>
-                    <span class="text">Dashboard</span>
-                    <span class="tooltip">Dashboard</span>
-                </a>
-            </li>
-            <li class="sidebar-list-item">
                 <a href="tasks.php">
                     <span class="material-symbols-outlined">task</span>
                     <span class="text">Tasks</span>
