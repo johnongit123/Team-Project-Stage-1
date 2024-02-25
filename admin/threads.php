6bfbc83e80223d707f2abd18a899ebacd244a398
johnongit123 <147746987+johnongit123@users.noreply.github.com>
2024-02-25 20:25:40 +0000

    Update threads.php

--- admin/threads.php
+++ admin/threads.php
@@ -155,8 +155,8 @@ require_once 'delete_threads.php';
                     </tbody>
                 </table>
             </div>
-            <div id="create-thread-container" class="form-container">
-                <div class="form-content">
+            <div id="create-thread-container" class="project-form-container">
+                <div class="project-form-content">
                     <h2>Create Thread</h2>
                     <div class="divider"></div>
                     <form id="thread-form" action="new_threads.php" method="post">
